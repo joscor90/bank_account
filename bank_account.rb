c0ae0111ce58b571329fefa5b3369c1172446dff
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-09 11:46:06 -0300

    Added public method withdraw to Account class

--- bank_account.rb
+++ bank_account.rb
@@ -4,16 +4,31 @@ class Account
       @name = name
       @balance = balance
     end
+
+    public
+    def withdraw(pin_number, amount)
+        if self.pin == pin_number
+            @balance -= amount
+            puts "Withdrew #{amount}. New balance: $#{@balance}"
+        else
+            puts self.pin_error
+        end
+    end
+
+    public
     def display_balance(pin_number)
         puts self.pin == pin_number ? "Balance: $#{@balance}." : self.pin_error
       end
     private 
+
     def pin
       @pin = 1234
     end
+
     private
     def pin_error
       "Access denied: incorrect PIN."
     end
+
 end
   
